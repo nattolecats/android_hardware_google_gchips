c73acb2d163f395a327e41483f5b939641ee2105
Devika Krishnadas <kdevika@google.com>
2023-06-21 22:50:29 +0000

    gralloc4: Upgrade Allocator to AIDL2
    Bug: 287353739
    Test: atest VtsHalGraphicsMapperV4_0TargetTest
    Test: ag/24139814
    
    Change-Id: I9a47233ac1ae8ee3236c5d1437edf0e39fc13d5b
    Signed-off-by: Devika Krishnadas <kdevika@google.com>

--- gralloc4/src/aidl/GrallocAllocator.h
+++ gralloc4/src/aidl/GrallocAllocator.h
@@ -2,9 +2,11 @@
 
 #include <aidl/android/hardware/graphics/allocator/AllocationResult.h>
 #include <aidl/android/hardware/graphics/allocator/BnAllocator.h>
+#include <aidl/android/hardware/graphics/allocator/BufferDescriptorInfo.h>
 #include <aidlcommonsupport/NativeHandle.h>
 
 #include <cstdint>
+#include <string>
 #include <vector>
 
 namespace pixel {
@@ -20,6 +22,15 @@ public:
 
     virtual ndk::ScopedAStatus allocate(const std::vector<uint8_t>& descriptor, int32_t count,
                                         AidlAllocator::AllocationResult* result) override;
+
+    virtual ndk::ScopedAStatus allocate2(const AidlAllocator::BufferDescriptorInfo& descriptor,
+                                         int32_t count,
+                                         AidlAllocator::AllocationResult* result) override;
+
+    virtual ndk::ScopedAStatus isSupported(const AidlAllocator::BufferDescriptorInfo& descriptor,
+                                           bool* result) override;
+
+    virtual ndk::ScopedAStatus getIMapperLibrarySuffix(std::string* result) override;
 
     virtual binder_status_t dump(int fd, const char** args, uint32_t numArgs) override;
 };
